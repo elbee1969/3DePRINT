1f152fd26fb076ab18d86732adaee831a4b0f35d
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2020-01-24 19:01:07 +0100

    in progress

--- frontChefdoeuvre/Front3DEPrint/src/app/classes/user-detail.ts
+++ frontChefdoeuvre/Front3DEPrint/src/app/classes/user-detail.ts
@@ -1,3 +1,15 @@
 export class UserDetail {
-    
+    emailId : string;
+    civility : string;
+    firstName : string;
+    lastName : string;
+    dateOfBirth : Date;
+    password : string;
+    confirPassword : string;
+    streetNumber : number;
+    street : string;
+    town : string;
+    zipCode : number;
+    country : string;
+    role : string;
 }
--- frontChefdoeuvre/Front3DEPrint/src/app/signup/signup.component.ts
+++ frontChefdoeuvre/Front3DEPrint/src/app/signup/signup.component.ts
@@ -32,6 +32,68 @@ form = new FormGroup({
   zipCode : new FormControl(''),
   country : new FormControl(''),  
   role: new FormControl('', Validators.required)
-})
+});
 
+UserForm(UserInformation){
+  let pwd = this.Password.value;
+  let confirmPwd = this.ConfirmPassword.value;
+
+  if(pwd == confirmPwd){
+    this.userDetail.emailId = this.Email.value;
+    this.userDetail.civility = this.Civility.value;
+    this.userDetail.firstName = this.FirstName.value;
+    this.userDetail.lastName = this.LastName.value;
+    this.userDetail.dateOfBirth = this.DateOfBirth.value;
+    this.userDetail.password = this.Password.value;
+    this.userDetail.streetNumber = this.StreetNumber.value;
+    this.userDetail.street = this.Street.value;
+    this.userDetail.town = this.Town.value;
+    this.userDetail.zipCode = this.ZipCode.value;
+    this.userDetail.country = this.Country.value;
+    this.userDetail.role = this.Role.value;
+  }
+
+}
+get Email(){
+  return this.form.get('email');
+}
+get Civility(){
+  return this.form.get('civility');
+}
+get FirstName(){
+  return this.form.get('firstName');
+}
+get LastName(){
+  return this.form.get('lastName');
+}
+get DateOfBirth(){
+  return this.form.get('dateOfBirth');
+}
+get Password(){
+  return this.form.get('password');
+}
+get ConfirmPassword(){
+  return this.form.get('confirmPassword');
+}
+get StreetNumber(){
+  return this.form.get('streetnumber');
+}
+get Street(){
+  return this.form.get('street');
+}
+get Town(){
+  return this.form.get('town');
+}
+get ZipCode(){
+  return this.form.get('zipCode');
+}
+get Country(){
+  return this.form.get('country');
+}
+get Role(){
+  return this.form.get('role');
+}
+get (){
+  return this.form.get('');
+}
 }
