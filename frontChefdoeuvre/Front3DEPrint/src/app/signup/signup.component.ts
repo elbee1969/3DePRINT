c1ecfc218730a8429ce77aa462b4e18836ef4833
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2020-01-24 17:33:52 +0100

    in progress

--- frontChefdoeuvre/Front3DEPrint/src/app/signup/signup.component.ts
+++ frontChefdoeuvre/Front3DEPrint/src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDetail } from '../classes/user-detail';
+import { UserService } from '../services/user.service';
+import { RouterModule } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  private userDetail = new UserDetail();
+
+  constructor(private userService : UserService, private router : RouterModule) { }
 
   ngOnInit() {
   }
+// form object creation
+form = new FormGroup({
+  email : new FormControl('', Validators.required),
+  civility : new FormControl('', Validators.required),
+  firstName : new FormControl('', Validators.required),
+  lastName : new FormControl('', Validators.required),
+  dateOfBirth : new FormControl('', Validators.required),
+  password : new FormControl('', Validators.required),
+  confirmPassword : new FormControl('', Validators.required),
+  streetNumber : new FormControl(''),
+  street : new FormControl(''),
+  town : new FormControl(''),
+  zipCode : new FormControl(''),
+  country : new FormControl(''),  
+  role: new FormControl('', Validators.required)
+})
 
 }
