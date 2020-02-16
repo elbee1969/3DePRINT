a674993a1a099a1030a1a7f8663c12d549f95622
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2020-02-16 20:13:01 +0100

    ajout du back

---
+++ back3DePrint/src/main/java/fr/formation/back3DePrint/Back3DePrintApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.back3DePrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back3DePrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Back3DePrintApplication.class, args);
+	}
+
+}
---
+++ back3DePrint/src/test/java/fr/formation/back3DePrint/Back3DePrintApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.back3DePrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back3DePrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
